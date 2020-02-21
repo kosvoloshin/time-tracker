f316b97645ccf998df4ff9a253f7a0041069467e
Konstantin Voloshyn <23.03@i.ua>
2020-02-21 14:01:30 +0200

    run build && added README

---
+++ build/precache-manifest.bdf6d09f856758af93c6f053a9182f8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03019388fd083f463cad4b92c522ffa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71fd4b34818152ab2d9d",
+    "url": "/static/css/main.d157117e.chunk.css"
+  },
+  {
+    "revision": "b6f5232e48acbc36e160",
+    "url": "/static/js/2.840c1824.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.840c1824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71fd4b34818152ab2d9d",
+    "url": "/static/js/main.6dc3bd9c.chunk.js"
+  },
+  {
+    "revision": "73a3af44da4ab7237b0c",
+    "url": "/static/js/runtime-main.c2956688.js"
+  }
+]);
